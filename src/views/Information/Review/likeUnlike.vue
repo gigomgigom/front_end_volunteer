7f4146c76b164e421e7cc7a4533b64dc5926d78a
java_jth <zc6171@gmail.com>
2024-06-19 20:31:54 +0900

    component add

---
+++ src/views/Information/Review/likeUnlike.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+      <div class="card-header">추천/비추천</div>
+      <div class="card-body">
+        
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
