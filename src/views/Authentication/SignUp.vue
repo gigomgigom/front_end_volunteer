e43990ca959271a0a3a39125af8d48b28190b93c
HwangSerim <32187719@dankook.ac.kr>
2024-06-24 17:00:01 +0900

    주소 찾기 api 추가

--- src/views/Authentication/SignUp.vue
+++ src/views/Authentication/SignUp.vue
@@ -68,14 +68,16 @@
         <div>
           <div class="d-flex align-items-center mb-3">
             <div class="d-flex justify-content-center align-items-center w-100">
-              <input type="text" class="form-control form-control-sm me-2" placeholder="우편번호">
-              <button class="btn btn-danger btn-sm me-2">우편번호 찾기</button>
+              <input type="text" class="form-control form-control-sm me-2"
+              id="sample6_postcode" placeholder="우편번호">
+              <button class="btn btn-danger btn-sm me-2"  @click="sample6_execDaumPostcode()"
+              >우편번호 찾기</button>
             </div>
-            <span class="verification-text">8자리 이상 16자리 이하로 입력</span>
+            <span class="verification-text"></span>
           </div>
           <div class="d-flex align-items-center">
-            <input type="text" class="form-control form-control-sm me-2 custom-input " placeholder="주소 입력">
-            <input type="text" class="form-control form-control-sm me-2 custom-input " placeholder="상세 주소 입력">
+            <input type="text" class="form-control form-control-sm me-2 custom-input "  id="sample6_address" placeholder="주소 입력">
+            <input type="text" class="form-control form-control-sm me-2 custom-input " id="sample6_detailAddress" placeholder="상세 주소 입력">
           </div>
         </div>
       </dd>
@@ -195,10 +197,76 @@
 
 <script setup>
 import TextHeader from '@/components/Common/TextHeader.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from "vue";
 import HighlightButton from '@/components/Common/HighlightButton.vue';
 
 const birthDate = ref(null);
+
+onMounted(() => {
+    if (!window.daum) {
+        loadScript();
+    }
+});
+
+function loadScript() {
+    const script = document.createElement("script");
+    script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    script.onload = () => {};
+
+    document.head.appendChild(script);
+}
+
+function sample6_execDaumPostcode() {
+    new window.daum.Postcode({
+        oncomplete: function (data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            var addr = ""; // 주소 변수
+            var extraAddr = ""; // 참고항목 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === "R") {
+                // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else {
+                // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+            if (data.userSelectedType === "R") {
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+                    extraAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if (data.buildingName !== "" && data.apartment === "Y") {
+                    extraAddr += extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
+                }
+                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if (extraAddr !== "") {
+                    extraAddr = " (" + extraAddr + ")";
+                }
+                 // 조합된 참고항목을 해당 필드에 넣는다.
+                 const extraAddressElement = document.getElementById("sample6_extraAddress");
+                        if (extraAddressElement) {
+                            extraAddressElement.value = extraAddr;
+                        }
+          
+                      }
+            
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById("sample6_postcode").value = data.zonecode;
+            document.getElementById("sample6_address").value = addr;
+            // 커서를 상세주소 필드로 이동한다.
+            document.getElementById("sample6_detailAddress").focus();
+        },
+    }).open();
+}
 </script>
 
 <style scoped>
